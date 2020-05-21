3f18ab06eec4d2cb26020e65f1782aa67889e2a6
Kellis <34947108+Gumbraise@users.noreply.github.com>
2020-05-21 23:45:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var schedule = require('node-schedule');
 const client = new Discord.Client();
 var prefix = ("'");
 
-client.login("NzEzMDIxOTEwMjQ1NDQxNTU2.XsaEJw.OeV8N2tKfTphp4aXIqr_ZFEME6E");
+client.login("hidden_token");
 
 client.on("ready", () => {
     client.user.setActivity("Ramadan mubrak", {type: "WATCHING"});
 
     const channel = client.channels.get('598206883106652176');
-    channel.send('isibe fi omok el 9a7ba');
+    //channel.send('isibe fi omok el 9a7ba');
 
     console.log('On Discord!');
 });
@@ -21,47 +21,53 @@ client.on("message", message => {
     if(!message.content.startsWith(prefix)) return;
 
     switch (args[0].toLowerCase()){
-        case "aide":
-            var client_creation = client.user.createdAt.toString().split(" ");
-            var aide = new Discord.RichEmbed()
-                .setColor("#FFFF00")
+        case "help":
+        default:
+            var help = new Discord.RichEmbed()
+                .setColor("#992D22")
                 .setThumbnail(client_icone)
-                .setTitle("Fiche de renseignement sur le BOT")
-                .addField("Liste des commandes", "**- //aide** *pour voir cette fiche.*\n **- //report** *pour envoyer un signalement à notre support.*\n **- //infoserveur** *pour voir les informations sur le serveur.*\n **- //infojoueur** *pour voir tes informations de ton compte Discord.*")
-                .addField("Liste des commandes admin", "**- //info** *pour envoyer un message depuis le serveur de gestion dans le channel information.*\n **- //msg** *pour envoyer un message important à un/des joueur(s).*")
-                .addField("A propos", "Ce BOT aide à l'orguanisation du serveur. Il est unique ! Vous ne le verrez que sur ce serveur *(ou sur ses partenariat)* !\n Ce BOT a été créer le " + client_creation[0] + " " + client_creation[2] + " " + client_creation[1] + " " + client_creation[3])
-                .setFooter(`Ati Community | version : ${client_version}`)
-                .setTimestamp()
+                .setTitle("مساعدة")
+                .addField(
+                    "Liste des commandes", 
+                    "**• 'help** *affiche le message d'aide.*")
+                
+                .addField(
+                    "Liste des commandes scam",
+                    "**• 'scam <numéro>** *cherche dans nos bases de données si le numéro est une arnaque.*\n **• 'scam add <numéro>** *ajoute un numéro de téléphone à notre base de données d'arnaques.*")
 
-            message.delete();
-            message.author.createDM().then(channel => {
-                channel.send(aide)
-            });
-        break;
+                .setFooter(`WinTea | Tea, Org.`)            
+            message.channel.send(help);
+            break;
 
-        case "report":
-            var report_find_args = message.content.split(" ").slice(1);
-            var report_args = report_find_args.join(" ");
-
-            if(!report_args) return message.reply("Veuillez inséré un motif.")
+        case "scam":
+            var help_arg_list = message.content.split(" ").slice(1);
+            var help_arg = help_arg_list.join(" ");
+            
+            switch (help_arg_list[0]) {
+                case 'add':
+                    var add_arg = help_arg_list.slice(1).join(" ");
+                    message.channel.send(add_arg);
+                    break;
+                default:
+                    message.channel.send(help_arg);
+                    break;
+            }
             
             var report_embed_joueur = new Discord.RichEmbed()
-                .setColor("#FFFF00")
+                .setColor("#992D22")
                 .setTitle("Report - Ati Community")
                 .setDescription("Votre report a été envoyer au support du serveur. Vous receverez une réponse sous peu.")
                 .addField("Motif du report", report_args)
                 .setFooter("Ati Community")
                 .setTimestamp()
-            
+                default:
             message.delete();
             client.channels.findAll('name', 'report').map(channel => channel.send(report_embed_admin));
             message.author.createDM().then(channel => {
                 channel.send(report_embed_joueur)
             });
-        break;
-        default:
-            
             break;
+        
     }
 });
 
@@ -73,4 +79,12 @@ var j = schedule.scheduleJob('0 0 0 * * *', function(){
 var k = schedule.scheduleJob('0 16 2 * * *', function(){
     const channel = client.channels.get('598206883106652176');
     channel.send('miboun time');
+});
+var l = schedule.scheduleJob('0 44 6 * * *', function(){
+    const channel = client.channels.get('598206883106652176');
+    channel.send('onoff time');
+});
+var m = schedule.scheduleJob('0 56 7 * * *', function(){
+    const channel = client.channels.get('598206883106652176');
+    channel.send('onoff time');
 });
